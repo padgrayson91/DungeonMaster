0cecca82e2ad6bff911bd53550a018d1b6422086
padgrayson91 <patrickgrayson12@gmail.com>
2019-01-31 16:06:03 -0600

    Cleanup

--- app/src/main/java/com/tendebit/dungeonmaster/charactercreation/model/fulfillment/Fulfillment.kt
+++ app/src/main/java/com/tendebit/dungeonmaster/charactercreation/model/fulfillment/Fulfillment.kt
@@ -3,6 +3,7 @@ package com.tendebit.dungeonmaster.charactercreation.model.fulfillment
 import com.tendebit.dungeonmaster.charactercreation.model.requirement.Requirement
 
 interface Fulfillment<T, StateType> {
+
 	val requirement: Requirement<T>
 
 	fun applyToState(state: StateType): Boolean
