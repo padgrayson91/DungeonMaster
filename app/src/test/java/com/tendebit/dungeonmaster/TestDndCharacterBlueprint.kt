ddeccaef91dc686d7e9f53be5c231ec7f2b7d2d4
padgrayson91 <patrickgrayson12@gmail.com>
2019-02-08 13:39:00 -0600

    Add a few more unit tests

--- app/src/test/java/com/tendebit/dungeonmaster/TestDndCharacterBlueprint.kt
+++ app/src/test/java/com/tendebit/dungeonmaster/TestDndCharacterBlueprint.kt
@@ -3,13 +3,16 @@ package com.tendebit.dungeonmaster
 import com.tendebit.dungeonmaster.charactercreation.feature.DndCharacterBlueprint
 import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndClassOptionsRequirement
 import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndClassRequirement
+import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndProficiencyOptionsRequirement
 import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndRaceOptionsRequirement
 import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndRaceRequirement
 import com.tendebit.dungeonmaster.charactercreation.feature.requirement.Requirement
 import com.tendebit.dungeonmaster.testhelpers.CharacterCreationRobots
 import com.tendebit.dungeonmaster.testhelpers.ValueRobot
+import io.reactivex.Observer
 import io.reactivex.observers.TestObserver
 import org.junit.Test
+import kotlin.reflect.KClass
 
 class TestDndCharacterBlueprint {
 
@@ -43,19 +46,51 @@ class TestDndCharacterBlueprint {
 	fun testRaceRequirementPresentAfterOptionsLoad() {
 		val toTest = DndCharacterBlueprint()
 		val testObserver = TestObserver<List<Requirement<*>>>()
-		toTest.requirements.subscribe(testObserver)
+		awaitRequirement(DndRaceRequirement::class, toTest, testObserver)
+
+		testObserver.assertTerminated()
+	}
+
+	@Test
+	fun testClassRequirementPresentAfterRaceSelected() {
+		val toTest = DndCharacterBlueprint()
+		val testObserver = TestObserver<List<Requirement<*>>>()
+		awaitRequirement(DndClassRequirement::class, toTest, testObserver)
+
+		testObserver.assertTerminated()
+	}
+
+	@Test
+	fun testProficiencyOptionsRequirementPresentAfterClassSelected() {
+		val toTest = DndCharacterBlueprint()
+		val testObserver = TestObserver<List<Requirement<*>>>()
+		awaitRequirement(DndProficiencyOptionsRequirement::class, toTest, testObserver)
+
+		testObserver.assertTerminated()
+	}
+
+	@Test
+	fun testProficiencyRequirementPresentAfterOptionsProvided() {
+		val toTest = DndCharacterBlueprint()
+		val testObserver = TestObserver<List<Requirement<*>>>()
+		awaitRequirement(DndProficiencyOptionsRequirement::class, toTest, testObserver)
+
+		testObserver.assertTerminated()
+	}
+
+	private fun awaitRequirement(ofType: KClass<*>, toTest: DndCharacterBlueprint, observer: Observer<List<Requirement<*>>>) {
+		toTest.requirements.subscribe(observer)
 		toTest.requirements.subscribe {
 			for (requirement in it) {
-				if (requirement is DndRaceRequirement) {
+				if (requirement::class == ofType) {
 					toTest.destroy()
+					break
 				}
 				if (requirement.status != Requirement.Status.FULFILLED) {
 					CharacterCreationRobots.runRobotForRequirement(requirement, ValueRobot.TestingLevel.STANDARD)
 				}
 			}
 		}
-
-		testObserver.assertTerminated()
 	}
 
 }
--- app/src/test/java/com/tendebit/dungeonmaster/testhelpers/CharacterCreationRobots.kt
+++ app/src/test/java/com/tendebit/dungeonmaster/testhelpers/CharacterCreationRobots.kt
@@ -1,9 +1,15 @@
 package com.tendebit.dungeonmaster.testhelpers
 
 import com.tendebit.dungeonmaster.charactercreation.feature.DndClass
+import com.tendebit.dungeonmaster.charactercreation.feature.DndProficiency
+import com.tendebit.dungeonmaster.charactercreation.feature.DndProficiencyGroup
 import com.tendebit.dungeonmaster.charactercreation.feature.DndRace
 import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndClassOptionsRequirement
+import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndClassRequirement
+import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndProficiencyOptionsRequirement
+import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndProficiencyRequirement
 import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndRaceOptionsRequirement
+import com.tendebit.dungeonmaster.charactercreation.feature.requirement.DndRaceRequirement
 import com.tendebit.dungeonmaster.charactercreation.feature.requirement.Requirement
 
 object CharacterCreationRobots {
@@ -13,6 +19,12 @@ object CharacterCreationRobots {
 		val robot = when(requirement) {
 			is DndRaceOptionsRequirement -> SimpleRobot(listOf(DndRace("Orc", "example.com/orc")))
 			is DndClassOptionsRequirement -> SimpleRobot(listOf(DndClass("Barbarian", "example.com/barbarian")))
+			is DndRaceRequirement -> SimpleRobot(DndRace("Orc", "example.com/orc"))
+			is DndClassRequirement -> SimpleRobot(DndClass("Barbarian", "example.com/barbarian"))
+			is DndProficiencyOptionsRequirement -> SimpleRobot(listOf(
+					DndProficiencyGroup(listOf(DndProficiency("Stealth", "example.com/stealth")), arrayListOf(), 1)
+																	 ))
+			is DndProficiencyRequirement -> SimpleRobot(requirement.fromGroup.availableOptions[0])
 			else -> null
 		} as? ValueRobot<T>
 		if (robot != null) {
