4e3525273611f0e567612d0c8c901df3d13fc86b
padgrayson91 <patrickgrayson12@gmail.com>
2019-02-25 14:36:14 -0600

    Generic selection viewmodel

--- app/src/main/java/com/tendebit/dungeonmaster/charactercreation/SelectionViewModel.kt
+++ app/src/main/java/com/tendebit/dungeonmaster/charactercreation/SelectionViewModel.kt
@@ -6,7 +6,7 @@ import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.BehaviorSubject
 
-open class SelectionViewModel<T> {
+class SelectionViewModel<T> {
 
 	private var internalSelectionRequirement: SelectionRequirement<T>? = null
 	var selectionRequirement
