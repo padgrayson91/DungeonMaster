56f3493fbdaed9d8cd61e1a2a232cb2f2ffbe772
padgrayson91 <patrickgrayson12@gmail.com>
2019-06-12 09:50:25 -0500

    Eliminate redundant suspend modifiers + fix ID related bug for class DB

--- app/src/main/java/com/tendebit/dungeonmaster/charactercreation3/characterclass/data/storage/RoomCharacterClassStorage.kt
+++ app/src/main/java/com/tendebit/dungeonmaster/charactercreation3/characterclass/data/storage/RoomCharacterClassStorage.kt
@@ -4,73 +4,51 @@ import com.tendebit.dungeonmaster.charactercreation3.Normal
 import com.tendebit.dungeonmaster.charactercreation3.Selected
 import com.tendebit.dungeonmaster.charactercreation3.characterclass.DndCharacterClass
 import com.tendebit.dungeonmaster.charactercreation3.characterclass.DndCharacterClassSelection
+import com.tendebit.dungeonmaster.charactercreation3.characterclass.logger
 import com.tendebit.dungeonmaster.charactercreation3.storage.CharacterCreationDb
 import com.tendebit.dungeonmaster.core.concurrency.Concurrency
 import io.reactivex.Maybe
 import io.reactivex.subjects.MaybeSubject
-import java.util.LinkedList
 import java.util.UUID
 
 class RoomCharacterClassStorage(private val database: CharacterCreationDb, private val concurrency: Concurrency) : DndCharacterClassStorage {
 
-	private val storageQueue = LinkedList<Pair<CharSequence, DndCharacterClassSelection>>()
-	private val readQueue = LinkedList<Pair<CharSequence, MaybeSubject<DndCharacterClassSelection>>>()
-
 	override fun storeSelection(selection: DndCharacterClassSelection, id: CharSequence?): CharSequence {
 		val createdOrExistingId = id ?: UUID.randomUUID().toString()
+		logger.writeDebug("Got request to store class selection with $createdOrExistingId")
 
-		synchronized(storageQueue) {
-			storageQueue.add(Pair(createdOrExistingId, selection))
-		}
-
-		concurrency.runDiskOrNetwork(::doStoreSelection)
+		concurrency.runDiskOrNetwork({
+			logger.writeDebug("Storing class selection to DB with ID $createdOrExistingId")
+			database.classDao().storeClassSelection(StoredCharacterClassSelection(createdOrExistingId.toString()))
+			val classesToStore = selection.options
+			for (state in classesToStore) {
+				val stateAsInt = if (state is Selected) 1 else 0
+				val dndClass = state.item ?: continue
+				database.classDao().storeClassInfo(StoredCharacterClass(dndClass.detailsUrl, dndClass.name))
+				database.classDao().storeSelectionClassJoin(StoredSelectionCharacterClassJoin(dndClass.detailsUrl, createdOrExistingId.toString(), stateAsInt))
+			}
+		})
 		return createdOrExistingId
 	}
 
 	override fun findSelectionById(id: CharSequence): Maybe<DndCharacterClassSelection> {
 		val subject = MaybeSubject.create<DndCharacterClassSelection>()
+		logger.writeDebug("Got request to read class selection with $id")
 
-		synchronized(readQueue) {
-			readQueue.add(Pair(id, subject))
-		}
-
-		concurrency.runDiskOrNetwork(::doReadSelection)
+		concurrency.runDiskOrNetwork({
+			logger.writeDebug("Reading class selection from DB with ID $id")
+			val classesForSelection = database.classDao().getClassesForSelection(id.toString())
+			if (classesForSelection.isEmpty()) {
+				logger.writeDebug("Did not have any available classes matching $id")
+				subject.onComplete()
+				return@runDiskOrNetwork
+			}
+			val selectedClass = database.classDao().getSelectedClassForSelection(id.toString())
+			val dndClasses = classesForSelection.map { DndCharacterClass(it.name, it.id) }
+			val states = dndClasses.map { if (it.detailsUrl == selectedClass?.id) Selected(it) else Normal(it) }
+			subject.onSuccess(DndCharacterClassSelection(states))
+		})
 		return subject
-	}
-
-	private suspend fun doStoreSelection() {
-		val nextRead = synchronized(storageQueue) {
-			storageQueue.removeAt(0)
-		}
-
-		val id = nextRead.first
-		val selection = nextRead.second
-		database.classDao().storeClassSelection(StoredCharacterClassSelection(id.toString()))
-		val classesToStore = selection.options
-		for (state in classesToStore) {
-			val stateAsInt = if (state is Selected) 1 else 0
-			val dndClass = state.item ?: continue
-			database.classDao().storeClassInfo(StoredCharacterClass(dndClass.detailsUrl, dndClass.name))
-			database.classDao().storeSelectionClassJoin(StoredSelectionCharacterClassJoin(dndClass.detailsUrl, id.toString(), stateAsInt))
-		}
-	}
-
-	private suspend fun doReadSelection() {
-		val nextRead = synchronized(readQueue) {
-			readQueue.removeAt(0)
-		}
-
-		val id = nextRead.first
-		val subject = nextRead.second
-		val classesForSelection = database.classDao().getClassesForSelection(id.toString())
-		if (classesForSelection.isEmpty()) {
-			subject.onComplete()
-			return
-		}
-		val selectedClass = database.classDao().getSelectedClassForSelection(id.toString())
-		val dndClasses = classesForSelection.map { DndCharacterClass(it.name, it.id) }
-		val states = dndClasses.map { if (it.detailsUrl == selectedClass?.id) Selected(it) else Normal(it) }
-		subject.onSuccess(DndCharacterClassSelection(states))
 	}
 
 }
