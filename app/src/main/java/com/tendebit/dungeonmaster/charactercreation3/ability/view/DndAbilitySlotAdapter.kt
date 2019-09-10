1729e0d219fae0e2dc5ed26eca652f78d3ac5414
padgrayson91 <patrickgrayson12@gmail.com>
2019-09-10 16:16:21 -0500

    Update dependencies

--- app/src/main/java/com/tendebit/dungeonmaster/charactercreation3/ability/view/DndAbilitySlotAdapter.kt
+++ app/src/main/java/com/tendebit/dungeonmaster/charactercreation3/ability/view/DndAbilitySlotAdapter.kt
@@ -37,7 +37,7 @@ class DndAbilitySlotAdapter(private val viewModel: DndAbilitySelectionViewModel?
 		childDisposable?.dispose()
 	}
 
-	fun resume() {
+	private fun resume() {
 		if (viewModel != null) {
 			mainDisposable = viewModel.changes.subscribe {
 				subscribeToSelection(it)
